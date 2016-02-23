29834fe3cb46154ab20ca756529e5a30c443d0fa
RayBuhr <ray@beermaster.beer.mids.ucb>
2016-02-22 22:45:35 -0600

    moved notes on PPI to own doc FRED_PPI_notes.md

--- data/FRED_other_beer_data_sources.R
+++ data/FRED_other_beer_data_sources.R
@@ -2,45 +2,8 @@
 # More FRED data 
 # ----------------------------------------------------------
 
-# Context and background notes for Price Index data:
-
-# What is a Producer Price Index?
-# From https://en.wikipedia.org/wiki/Producer_price_index
-# A Producer Price Index (PPI) measures the average changes in prices received by 
-# domestic producers for their output. It is one of several price indices.
-
-# From http://www.bls.gov/ppi/data.htmhttp://www.bls.gov/ppi/data.htm
-
-# The Producer Price Index (PPI) program measures the average change over time in 
-# the selling prices received by domestic producers for their output. The prices 
-# included in the PPI are from the first commercial transaction for many products 
-# and some services.
-
-# From http://www.bls.gov/ppi/ppifaq.htm#13
-# There are three main PPI classification structures which draw from the same pool 
-# of price information provided to the BLS by cooperating company survey respondents:
-
-# - Industry classification. A Producer Price Index for an industry is a measure 
-# of changes in prices received for the industry's output sold outside the industry 
-# (that is, its net output). The PPI publishes approximately 535 industry price 
-# indexes in combination with over 4,000 specific product line and product category 
-# sub-indexes, as well as, roughly 500 indexes for groupings of industries. North 
-# American Industry Classification System (NAICS) index codes provide comparability 
-# with a wide assortment of industry-based data for other economic programs, 
-# including productivity, production, employment, wages, and earnings.
-
-# - Commodity classification. The commodity classification structure of the PPI 
-# organizes products and services by similarity or material composition, regardless 
-# of the industry classification of the producing establishment. This system is 
-# unique to the PPI and does not match any other standard coding structure. In all, 
-# PPI publishes more than 3,700 commodity price indexes for goods and about 800 for 
-# services (seasonally adjusted and not seasonally adjusted), organized by product, 
-# service, and end use.
-
-# Commodity-based Final Demand-Intermediate Demand (FD-ID) System. Commodity-based 
-# FD-ID price indexes regroup commodity indexes for goods, services, and construction
-# at the subproduct class (six-digit) level, according to the type of buyer and the 
-# amount of physical processing or assembling the products have undergone. 
+# Context and background notes for Price Index data located 
+# in root at FRED_PPI_notes.md
 
 library(dplyr)
 library(fredr)
